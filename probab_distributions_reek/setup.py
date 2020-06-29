6ac8641d59d1db610557791d341e2afa2f2e801c
Akhil Sanker <42499275+reekithak@users.noreply.github.com>
2020-06-30 02:58:43 +0530

    Add files via upload

---
+++ probab_distributions_reek/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probab_distributions_reek',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probab_distributions_reek'],
+      zip_safe=False)
