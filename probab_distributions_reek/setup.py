3e686528372a5669eeef151c3306b82bc64da664
Akhil Sanker <42499275+reekithak@users.noreply.github.com>
2020-06-30 02:59:24 +0530

    Update setup.py

--- probab_distributions_reek/setup.py
+++ probab_distributions_reek/setup.py
@@ -4,4 +4,5 @@ setup(name='probab_distributions_reek',
       version='0.1',
       description='Gaussian distributions',
       packages=['probab_distributions_reek'],
-      zip_safe=False)
+      zip_safe=False,
+     download_url = "https://github.com/reekithak/probab_distributions_reek/archive/0.1.tar.gz")
